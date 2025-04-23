ce682a1fbd2042a0e4d5fab4fc2534ff7d308925
Nicolas zequea <nicomansolov89@gmail.com>
2025-04-22 23:06:51 -0500

    nuevo commit en la rama principal

--- Plan mejoramiento/src/components/Inicio/Inicio.jsx
+++ Plan mejoramiento/src/components/Inicio/Inicio.jsx
@@ -5,43 +5,57 @@ function Inicio() {
 
   return (
     <>
+
     <header className='Cabecera'>
     <div className='Titulo'>
         <h1>Titulo</h1>
         </div>
-        <div className='Text'>
+        <div className='Links'>
             <Link to="/PerfilEmpresa">Perfil Empresa</Link>
             <Link to="/contacto">Contacto</Link>
             <Link to="/sobrenosotros">Sobre Nosotros</Link>
           </div>
-    </header>
-    <div className='Container'>
-      <div className='NavPrincip'>
-          <Link to="/login" className='Button'>Login</Link>
+        <div className='Button'>
+          <Link to="/Login">Login</Link>
         </div>
-      <header className='header'>
-        <h2>Website Design</h2>
-        <h2>Template</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat ut cupiditate saepe similique eveniet consequatur dolore, ab placeat. Saepe, cum.</p>
-      </header>
+    </header>
+    
+    
+      <nav className='Navegacion'>
+        <div className='Tx1'>
+          <h2>Website Design</h2>
+        </div>
+        <div className='Tx2'>
+          <h2>Template</h2>
+        </div>
+        <p>Loremipsumdolorsitametconsecteturadipisicingelit
+        Loremipsumdolorsitametconsecteturadipisicingeli
+        inciduntquorecusandaenequeilloinciduntquor</p>
+        <p>Lorem ipsum dolor.</p>
+      </nav>
 
-      <section className='SeccionPrin'>
+      <main className='SeccionPrin'>
         <div className='Columna1'>
         <div className='circulo'><h3>Texto</h3></div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat ut cupiditate saepe similique eveniet consequatur dolore, ab placeat. Saepe, cum.</p>
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia Lorem ipsum incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat cupiditae similique eveniet consequatur dolore 
+          Lorem ipsum dolor sit ametadipisicing recusandae, neque illo labore fugiat totam simi
+          </p>
           <p className='TxtBib'>Isaias 41:10</p>
         </div>
         <div className='Columna2'>
         <div className='circulo'><h3>Texto</h3></div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat ut cupiditate saepe similique eveniet consequatur dolore, ab placeat. Saepe, cum.
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia Lorem ipsum incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat cupiditae similique eveniet consequatur dolore 
+          Lorem ipsum dolor sit ametadipisicing recusandae, neque illo labore fugiat totam simi
           </p>
           <p className='TxtBib'>Juan 14:6</p>
         </div>
           <div className='ImagePlaceHold'></div>
-      </section>
+      </main>
 
       <section className='SeccionTxt'>
-          <div className='Seccion2'>
+          <article className='Seccion2'>
             <h4>Texto</h4>
             <div className='Comentarios'>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandae, neque illo labore fugiat totam doloremque repellat ut cupiditate saepe similique eveniet consequatur dolore, ab placeat. Saepe, cum.</p>
@@ -58,14 +72,17 @@ function Inicio() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandaeLorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt quo recusandae,</p>
             </div>
             </div>
+          </article>
+          <section className='CAJAS'>
+          <div className='BOX'></div>
+          <div className='Cajitas'>
+            <div className='Caja'></div>
+            <div className='Caja'></div>
+            <div className='Caja'></div>
+            <div className='Caja'></div>
+            <div className='Caja'></div>
           </div>
-          <div className='BOX'>
-          <div className='Box'></div>
-              <div className='Box'></div>
-              <div className='Box'></div>
-              <div className='Box'></div>
-              <div className='Box'></div>
-          </div>
+        </section>
         </section>
 
         <section className='UltGenrl'>
@@ -78,7 +95,7 @@ function Inicio() {
           <div className='BOX4'></div>
         </section>
         </section>
-    </div>
+
     </>
   )
 }
